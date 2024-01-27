08543087d19fdf1fbd2c5cbbc607fb7193eb4c0d
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-01-27 11:10:41 +0530

    feat (new) : get note by id

--- controllers/notes-controller.js
+++ controllers/notes-controller.js
@@ -1,19 +1,26 @@
-const notes = require("../utils/data");
+const notesData = require("../utils/data");
 
 const allNotes = async (req, res) => {
   try {
-    if (!notes || notes.length === 0) {
+    if (!notesData || notesData.length === 0) {
       return res.status(404).json({ error: "No notes found." });
     }
 
-    res.json(notes);
+    res.json(notesData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
+const getNotesById = async (req, res) => {
+  try {
+    const note = notesData.find((n) => n.id === req.params.id);
+    res.send(note)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-
-
-module.exports = { allNotes };
+module.exports = { allNotes, getNotesById };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
-var express = require('express');
-const { allNotes } = require('../controllers/notes-controller');
+var express = require("express");
+const { allNotes, getNotesById } = require("../controllers/notes-controller");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/api/notes',allNotes)
+router.get("/api/notes", allNotes);
+
+router.get('/api/notes/:id',getNotesById)
 
 module.exports = router;
